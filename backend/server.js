1ea3995401f1757174c46b743e827599a5636048
sundararajan-git <sundararajanselvarasu@gmail.com>
2024-08-11 12:44:48 +0530

    spelling updated

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,28 @@
 require('dotenv').config()
 const express = require("express")
-const cros = require("cors")
-const app  = express()
+const cors = require("cors")
+const app = express()
 const mongoose = require("mongoose")
 const taskRoutes = require("./routes/taskRoutes")
 const PORT = process.env.PORT
-const URI = process.env.MANGO_URI
+const URI = process.env.MONGO_URI
 
 
 app.use(express.json())
-app.use(cros())
+app.use(cors())
 
-mongoose.connect(URI).then(()=>{
-    app.listen(PORT,(resq,res)=>{
-        console.log("DB connected and listsing on " , PORT)
+mongoose.connect(URI).then(() => {
+    app.listen(PORT, (resq, res) => {
+        console.log("DB connected and listsing on ", PORT)
     })
-}).catch((err)=>{
+}).catch((err) => {
     console.error(err.message)
 })
 
+app.use("/", taskRoutes)
 
-app.use("/",taskRoutes)
 
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
